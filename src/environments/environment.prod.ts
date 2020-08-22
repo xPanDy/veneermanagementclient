423776bf96ddc621e07b686a1e081ccf3a3f7b71
Kristaps Šūlmeisters <sulmeisterskristaps@gmail.com>
2020-08-22 14:53:37 +0300

    depl config2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 
 
-  production: false,
+  production: true,
   apiUrl:`https://glacial-tor-15246.herokuapp.com/`
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl:`https://glacial-tor-15246.herokuapp.com/`
+  apiUrl:`http://localhost:4200`
 };
 
 /*
